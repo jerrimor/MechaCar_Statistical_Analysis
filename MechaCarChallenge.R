9516a5716de89fb0e87b13539b1cd10d0578afa9
jerrimor <96222437+jerrimor@users.noreply.github.com>
2022-04-09 23:09:02 -0500

    Adding in Deliverable 2 scripts

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,13 @@ head(mecha_table)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_table) #mult linear regression model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_table)) #generate stats
+
+#Deliverable 2
+
+mecha_table2 <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F) #import suspension coil csv
+
+total_summary <- mecha_table2 %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), sd(PSI)) #create summary table with stats
+
+lot_summary <- mecha_table2 %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), sd(PSI)) #create summary table by lot
+
+
